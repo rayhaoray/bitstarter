b47d1cf0891ae87badfaef81afdbead84adf5e49
Ubuntu <ubuntu@ip-172-31-39-190.us-west-2.compute.internal>
2013-07-17 02:46:41 +0000

    Updated 8080 port

--- web.js
+++ web.js
@@ -2,7 +2,6 @@ var fs = require('fs');
 var express = require('express');
 var buffer = new Buffer(40);
 buffer = fs.readFileSync('index.html');
-console.log(buffer.toString());
 
 var app = express.createServer(express.logger());
 
@@ -10,7 +9,7 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
